0c92ffa0ede438ba09b60ca5af42ac02e70d4762
ozluuh <ozluuh@gmail.com>
2021-04-25 01:41:18 -0300

    feat: jpa persistence

--- src/main/java/br/com/carlover/bean/CarBean.java
+++ src/main/java/br/com/carlover/bean/CarBean.java
@@ -1,7 +1,10 @@
 package br.com.carlover.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
+import br.com.carlover.dao.CarDao;
 import br.com.carlover.model.Car;
 
 @ManagedBean
@@ -10,7 +13,9 @@ public class CarBean {
     private Car car = new Car();
 
     public void save() {
+        new CarDao().save(this.car);
         System.out.println("salvando..." + this.car);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Successfully registered"));
     }
 
     public Car getCar() {
--- src/main/java/br/com/carlover/model/Car.java
+++ src/main/java/br/com/carlover/model/Car.java
@@ -2,14 +2,32 @@ package br.com.carlover.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_CLR_CAR")
 public class Car {
-    
+
+    @Id
+    @Column(name = "id_control")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ds_model", length = 125, nullable = false)
     private String model;
-    
+
+    @Column(name = "ds_brand", length = 30, nullable = false)
     private String brand;
-    
+
+    @Column(name = "ds_car")
     private String description;
-    
+
+    @Column(name = "vl_car", nullable = false)
     private BigDecimal price;
 
     @Override
