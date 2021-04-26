733a806eafcca01b1a80892115ef0f73730264ac
ozluuh <ozluuh@gmail.com>
2021-04-25 22:46:00 -0300

    feat: users files

---
+++ src/main/java/br/com/carlover/bean/UserBean.java
@@ -0,0 +1,43 @@
+package br.com.carlover.bean;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import br.com.carlover.dao.UserDao;
+import br.com.carlover.model.User;
+
+@Named
+@RequestScoped
+public class UserBean {
+
+    private User user = new User();
+
+    private final LocalDate minDate = LocalDate.of(1900, 1, 1);
+
+    public void save() {
+        new UserDao().save(this.user);
+        System.out.println("Saving: " + this.user);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Successfully registered"));
+    }
+
+    public List<User> getUsers() {
+        return new UserDao().getAll();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getMinDate() {
+        return minDate;
+    }
+}
---
+++ src/main/java/br/com/carlover/model/User.java
@@ -0,0 +1,69 @@
+package br.com.carlover.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_CLR_USER")
+public class User {
+
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nm_user", nullable = false, length = 170)
+    private String name;
+
+    @Column(name = "dt_birthday", nullable = false)
+    private LocalDate birthday;
+
+    @Column(name = "ds_mail")
+    private String mail;
+
+    @Column(name = "ds_password", nullable = false)
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", birthday=" + birthday + ", mail=" + mail + ", password=" + password + "]";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+}
