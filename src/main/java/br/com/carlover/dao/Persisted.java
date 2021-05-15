22cb5f72e8013655a5f6b5455d79b61c7860ff94
ozluuh <ozluuh@gmail.com>
2021-05-15 17:59:57 -0300

    refactor: add dao persistence contract

---
+++ src/main/java/br/com/carlover/dao/Persisted.java
@@ -0,0 +1,42 @@
+package br.com.carlover.dao;
+
+import java.util.List;
+
+public interface Persisted<E, K> {
+
+    /**
+     * Persist entity
+     *
+     * @param entity entity instance
+     */
+    void save(E entity);
+
+    /**
+     * Update the entity
+     *
+     * @param entity entity instance
+     */
+    void update(E entity);
+
+    /**
+     * Find entity by id
+     *
+     * @param key id
+     * @return entity instance or null if not found
+     */
+    E findById(K key);
+
+    /**
+     * Find all entity
+     *
+     * @return list of entities
+     */
+    List<E> findAll();
+
+    /**
+     * Delete managed entity
+     *
+     * @param entity entity instance
+     */
+    void delete(E entity);
+}
