7dda168999b2f55001a52bc82d214aefc690f4c4
ozluuh <ozluuh@gmail.com>
2021-05-14 23:33:48 -0300

    feat: create update endpoint

--- src/main/java/br/com/carlover/rest/CarEndpoint.java
+++ src/main/java/br/com/carlover/rest/CarEndpoint.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,7 +22,6 @@ public class CarEndpoint {
     CarDao dao = new CarDao();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Car> index() {
         return dao.getAll();
     }
@@ -48,6 +48,15 @@ public class CarEndpoint {
         if (car == null) {
             return Response.status(Response.Status.NOT_FOUND).entity(car).build();
         }
+        return Response.status(Response.Status.OK).entity(car).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") Long id, Car car) {
+        car.setId(id);
+        dao.update(car);
         return Response.status(Response.Status.OK).entity(car).build();
     }
 }
