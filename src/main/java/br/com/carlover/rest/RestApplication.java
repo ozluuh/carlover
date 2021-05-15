54c4d6d7468345b811fb44931b0787a5ed4227fe
ozluuh <ozluuh@gmail.com>
2021-05-14 22:57:32 -0300

    feat: change endpoint root

--- src/main/java/br/com/carlover/rest/RestApplication.java
+++ src/main/java/br/com/carlover/rest/RestApplication.java
@@ -3,6 +3,6 @@ package br.com.carlover.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/rest")
+@ApplicationPath("/api")
 public class RestApplication extends Application {
 }
