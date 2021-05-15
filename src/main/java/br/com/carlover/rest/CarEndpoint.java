dd7aef9f08a5dc010f1eb3f60e29127dd5ffe1c5
ozluuh <ozluuh@gmail.com>
2021-05-14 22:57:48 -0300

    feat: create car endpoint

---
+++ src/main/java/br/com/carlover/rest/CarEndpoint.java
@@ -0,0 +1,42 @@
+package br.com.carlover.rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.com.carlover.dao.CarDao;
+import br.com.carlover.model.Car;
+
+@Path("/cars")
+public class CarEndpoint {
+
+    CarDao dao = new CarDao();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Car> index() {
+        return dao.getAll();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Car car) {
+        if (car == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        try {
+            dao.save(car);
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+
+        return Response.status(Response.Status.CREATED).entity(car).build();
+    }
+}
