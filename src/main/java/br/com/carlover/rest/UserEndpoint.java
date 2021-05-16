5b135b0940f1486cc6869ac42a5cb844007646e7
ozluuh <ozluuh@gmail.com>
2021-05-16 15:11:12 -0300

    feat(rest): add update endpoint

--- src/main/java/br/com/carlover/rest/UserEndpoint.java
+++ src/main/java/br/com/carlover/rest/UserEndpoint.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -47,4 +48,22 @@ public class UserEndpoint {
         }
         return Response.status(Response.Status.ACCEPTED).entity(entity).build();
     }
+
+    @PUT
+    @Path("{id}")
+    public Response update(@PathParam("id") Long id, User entity) {
+        User instance = dao.findById(id);
+        if (instance == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        entity.setId(id);
+        dao.update(entity);
+        try {
+            dao.commit();
+        } catch (CommitTransactionException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+
 }
