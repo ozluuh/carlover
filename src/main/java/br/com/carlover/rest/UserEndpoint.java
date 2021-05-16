6c68053f2634d1a5f80055dc149af8055b288e25
ozluuh <ozluuh@gmail.com>
2021-05-16 15:08:17 -0300

    feat(rest): add show endpoint

--- src/main/java/br/com/carlover/rest/UserEndpoint.java
+++ src/main/java/br/com/carlover/rest/UserEndpoint.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import br.com.carlover.connection.ConnectionFactory;
 import br.com.carlover.dao.UserDao;
@@ -21,5 +23,15 @@ public class UserEndpoint {
     @GET
     public List<User> index() {
         return dao.findAll();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response show(@PathParam("id") Long id) {
+        User entity = dao.findById(id);
+        if (entity == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.status(Response.Status.OK).entity(entity).build();
     }
 }
