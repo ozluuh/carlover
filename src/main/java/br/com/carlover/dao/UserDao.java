59ad1bdc0659cb44b0370ded4066de3c101aaa1e
ozluuh <ozluuh@gmail.com>
2021-05-15 23:17:39 -0300

    refactor(dao): convert to interface

--- src/main/java/br/com/carlover/dao/UserDao.java
+++ src/main/java/br/com/carlover/dao/UserDao.java
@@ -1,29 +1,6 @@
 package br.com.carlover.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-
-import br.com.carlover.connection.ConnectionFactory;
 import br.com.carlover.model.User;
 
-public class UserDao {
-
-    public void save(User user) {
-        EntityManager manager = ConnectionFactory.getConnection();
-
-        manager.getTransaction().begin();
-        manager.persist(user);
-        manager.getTransaction().commit();
-        manager.close();
-
-    }
-
-    public List<User> getAll() {
-        EntityManager manager = ConnectionFactory.getConnection();
-        String jpql = "SELECT u FROM User u";
-
-        return manager.createQuery(jpql, User.class).getResultList();
-    }
-
+public interface UserDao extends Persisted<User, Long> {
 }
