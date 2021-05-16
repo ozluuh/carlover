2a8dc4b9076125ef461e4a9d21f674c4432a9dcd
ozluuh <ozluuh@gmail.com>
2021-05-16 15:08:57 -0300

    feat(rest): add store endpoint

--- src/main/java/br/com/carlover/rest/UserEndpoint.java
+++ src/main/java/br/com/carlover/rest/UserEndpoint.java
@@ -3,6 +3,7 @@ package br.com.carlover.rest;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,6 +13,7 @@ import javax.ws.rs.core.Response;
 import br.com.carlover.connection.ConnectionFactory;
 import br.com.carlover.dao.UserDao;
 import br.com.carlover.dao.impl.UserDaoImpl;
+import br.com.carlover.exception.CommitTransactionException;
 import br.com.carlover.model.User;
 
 @Path("users")
@@ -33,5 +35,16 @@ public class UserEndpoint {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
         return Response.status(Response.Status.OK).entity(entity).build();
+    }
+
+    @POST
+    public Response store(User entity) {
+        dao.save(entity);
+        try {
+            dao.commit();
+        } catch (CommitTransactionException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+        return Response.status(Response.Status.ACCEPTED).entity(entity).build();
     }
 }
