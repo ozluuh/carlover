923a0b18b3940a948d3aeffdf34dbf863f26cc2d
ozluuh <ozluuh@gmail.com>
2021-05-21 22:35:28 -0300

    refactor: get user from context and redirect

--- src/main/java/br/com/carlover/util/AuthorizationListener.java
+++ src/main/java/br/com/carlover/util/AuthorizationListener.java
@@ -1,14 +1,24 @@
 package br.com.carlover.util;
 
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
+
+import br.com.carlover.model.User;
 
 public class AuthorizationListener implements PhaseListener {
 
     @Override
     public void afterPhase(PhaseEvent event) {
-        System.out.println("[PHASE] " + event.getPhaseId());
+        FacesContext context = FacesContext.getCurrentInstance();
+        User user = (User) context.getExternalContext().getSessionMap().get("user");
+
+        if (user == null) {
+            NavigationHandler navigation = context.getApplication().getNavigationHandler();
+            navigation.handleNavigation(context, "", "login?faces-redirect=true");
+        }
     }
 
     @Override
