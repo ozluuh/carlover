881386280f92581b7d5649cf36d60f10bccf1de2
ozluuh <ozluuh@gmail.com>
2021-05-28 04:06:13 -0300

    refactor: add find by user method

--- src/main/java/br/com/carlover/dao/CarDao.java
+++ src/main/java/br/com/carlover/dao/CarDao.java
@@ -1,6 +1,11 @@
 package br.com.carlover.dao;
 
+import java.util.List;
+
 import br.com.carlover.model.Car;
+import br.com.carlover.model.User;
 
 public interface CarDao extends Persisted<Car, Long> {
+
+    List<Car> findByUserId(User user);
 }
