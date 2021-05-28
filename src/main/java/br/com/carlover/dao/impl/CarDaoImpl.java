c3772fb4c91b055dfbb3be882f4d70f30c5cac5a
ozluuh <ozluuh@gmail.com>
2021-05-28 04:09:34 -0300

    feat: add implementation

--- src/main/java/br/com/carlover/dao/impl/CarDaoImpl.java
+++ src/main/java/br/com/carlover/dao/impl/CarDaoImpl.java
@@ -1,14 +1,25 @@
 package br.com.carlover.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.carlover.dao.CarDao;
 import br.com.carlover.model.Car;
+import br.com.carlover.model.User;
 
 public class CarDaoImpl extends PersistedImpl<Car, Long> implements CarDao {
 
     public CarDaoImpl(EntityManager manager) {
         super(manager);
+    }
+
+    @Override
+    public List<Car> findByUserId(User user) {
+        return manager
+                .createQuery("from Car c where c.user.id = :id", Car.class)
+                .setParameter("id", user.getId())
+                .getResultList();
     }
 
 }
